7788d281030db45d5c4397c8ce130dc51de784a6
Cory Bateman <splintercode.cb@gmail.com>
2015-01-16 15:33:26 -0600

    basic auth work

--- App/JavaScript/Src/controllers.js
+++ App/JavaScript/Src/controllers.js
@@ -3,51 +3,53 @@
 
     var appControllers = angular.module('app.controllers', []);
 
-    appControllers.controller('BaseCtrl', ['$firebase', '$scope', 'voteService', function($firebase, $scope, voteService) {
-
-        // Set up FireBase Connection
-        var dataRef = new Firebase("https://verify-it.firebaseio.com/data");
-        var sync = $firebase(dataRef);
-        var syncObject = sync.$asObject();  // download the data into a local object
-
-        // FireBase Models
-        syncObject.$bindTo($scope, "data");
-
+    appControllers.controller('BaseCtrl', ['$scope', '$firebase', 'userService', function($scope, $firebase, userService) {
         // View Models
         var vm = this;
-        vm.h1 = 'Verify It';
-
-
-
 
         //#region Authentication
         var authRef = new Firebase("https://verify-it.firebaseio.com");
-        var disconnectRef = {};
 
         authRef.authAnonymously(function(error, authData) {
-            if (error) {
-                console.log("Login Failed!", error);
+            if (authData) {
+                login(authData);
             } else {
-                console.log("User " + authData.uid + " is logged in with " + authData.provider);
-
-                authRef.child("users").child(authData.uid).set(authData); // Save user data 
-
-                var disconnectRef = new Firebase('https://verify-it.firebaseio.com/users/' + authData.uid);
-                disconnectRef.onDisconnect().remove();
+                console.log("Login Failed!", error);
             }
         });
 
         authRef.onAuth(function(authData) {
             if (authData) {
-                var userRef = new Firebase("https://verify-it.firebaseio.com/users");
-                var sync = $firebase(userRef);
-                vm.users = sync.$asObject();
-
-                //console.log("User " + authData.uid + " is logged in with " + authData.provider);
+                getOnlineUsers(authData);
             } else {
-                //console.log("User is logged out");
+                console.log("User logged out");
             }
         });
+
+        function login(authData) {
+            var userData = {
+                authData: authData,
+                userData: {
+                    name: 'anonymous'
+                }
+            };
+
+            var userRef = new Firebase('https://verify-it.firebaseio.com/users/' + authData.uid);
+            userRef.set(userData);              // Save user data 
+            userRef.onDisconnect().remove();    // Delete user data on end of session
+
+            var sync = $firebase(userRef);
+            var syncObject = sync.$asObject();  // download the data into a local object
+            syncObject.$bindTo($scope, "user"); // FireBase Data Models
+        }
+
+        function getOnlineUsers(authData) {
+            var userRef = new Firebase("https://verify-it.firebaseio.com/users");
+            var sync = $firebase(userRef);
+            vm.users = sync.$asObject();
+
+            console.log("User " + authData.uid + " is logged in with " + authData.provider);
+        }
         //#endregion
     }]);
 }());
