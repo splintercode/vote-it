e80b57a2a444427cc5e6a7370e6d1b7e8db66460
Cory Bateman <splintercode.cb@gmail.com>
2015-01-19 21:13:01 -0600

    group work
    Will need heavy refactoring. Abstract logic to services and test.

--- App/JavaScript/Src/controllers.js
+++ App/JavaScript/Src/controllers.js
@@ -3,10 +3,12 @@
 
     var appControllers = angular.module('app.controllers', []);
 
-    appControllers.controller('BaseCtrl', ['$scope', '$firebase', function($scope, $firebase) {
+    appControllers.controller('BaseCtrl', ['$scope', '$firebase', '$firebaseAuth', function($scope, $firebase, $firebaseAuth) {
         var fireBase = new Firebase('https://vote-it.firebaseio.com');
         var vm = this;
 
+        vm.name = 'Anonymous';
+        vm.showGroup = false;
         vm.voteTypes = [{
             value: 'Yes'
         }, {
@@ -15,41 +17,91 @@
             value: 'Neutral'
         }];
 
-        //#region Authentication
+        vm.join = function() {
+            vm.showGroup = true;
+
+            joinGroup(vm.name, vm.group);
+        };
+
         fireBase.authAnonymously(function(error, authData) {
-            if (authData) {
-                login(authData);
-            } else {
-                console.log('Login Failed!', error);
-            }
+            createSession(authData);
         });
 
         fireBase.onAuth(function() {
-            getUpdatedListOfUsers();
+            // Get update list of users
+            //var sync = $firebase(fireBase.child('users'));
+            //vm.users = sync.$asObject();
         });
 
-        function login(authData) {
-            var userData = {
-                authData: authData,
-                userData: {
-                    name: (vm.name ? vm.name : 'Anonymous'),
-                    vote: 'Neutral'
+        function joinGroup(userName, groupName) {
+            var auth = $firebaseAuth(fireBase);
+            var userAuth = auth.$getAuth();
+            var uid = userAuth.uid;
+
+            // store user
+            fireBase.child('/users/' + uid + '/userData/').update({
+                userName: userName,
+                groupName: groupName
+            });
+
+            // store group
+            fireBase.child('/groups/' + groupName).once('value', function(group) {
+                if (group.val() !== null) {
+                    console.log('Group Exists!');
+
+                    // Update user list with new user
+                    fireBase.child('/groups/' + groupName).child('/users/' + uid).set
+                        ({
+                            userName: userName,
+                            vote: 'Neutral'
+                        });
+
+                } else {
+                    console.log('Group Does not exist!');
+
+                    // Create group
+                    fireBase.child('/groups/' + groupName).set({
+                        groupName: groupName
+                    });
+
+                    // Update user list with new user
+                    fireBase.child('/groups/' + groupName + '/users/' + uid).set
+                    ({
+                        userName: userName,
+                        vote: 'Neutral'
+                    });
                 }
-            };
 
-            var userRef = new Firebase('https://vote-it.firebaseio.com/users/' + authData.uid);
-            userRef.set(userData);              // Save user data
-            userRef.onDisconnect().remove();    // Delete user data on end of session
+                // Remove user from user list on disconnect
+                fireBase.child('/groups/' + groupName + '/users/' + uid).onDisconnect().remove();
 
-            var sync = $firebase(userRef);
-            var syncObject = sync.$asObject();  // download the data into a local object
-            syncObject.$bindTo($scope, 'user'); // FireBase Data Models
+                // sync group to vm
+                var syncGroup = $firebase(fireBase.child('/groups/' + groupName + '/users/'));
+                vm.users = syncGroup.$asObject();
+
+                // sync user to vm
+                var syncUser = $firebase(fireBase.child('/groups/' + groupName + '/users/' + uid));
+                var syncObject = syncUser.$asObject();  // download the data into a local object
+                syncObject.$bindTo($scope, 'user'); // FireBase Data Models (3 way binding)
+            });
         }
 
-        function getUpdatedListOfUsers() {
-            var sync = $firebase(fireBase.child('users'));
-            vm.users = sync.$asObject();
+        function createSession(authData) {
+            if (authData) {
+                var userData = {
+                    authData: authData,
+                    userData: {
+                        userName: vm.name,
+                        groupName: ''
+                    }
+                };
+
+                var usersRef = fireBase.child('/users/' + authData.uid);
+                usersRef.set(userData);              // Save user data
+                usersRef.onDisconnect().remove();    // Delete user data on end of session
+            } else {
+                console.log('Login Failed!', error);
+            }
         }
-        //#endregion
     }]);
 }());
